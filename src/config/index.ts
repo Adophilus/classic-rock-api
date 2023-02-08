55454821a9b13b2cc5d506f0bfb14dc02848fd00
Adophilus <uchenna19of@gmail.com>
2023-02-08 17:15:04 +0100

    set default db path

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,7 +11,7 @@ const config = {
   },
   db: {
     uri: process.env.DB_URI ? process.env.DB_URI : "sqlite:///db.sqlite3",
-    path: process.env.DB_PATH ? process.env.DB_PATH : "",
+    path: process.env.DB_PATH ? process.env.DB_PATH : "db.sqlite3",
     dialect: process.env.DB_DIALECT ? String(process.env.DB_DIALECT) as Dialect : defaultDialect,
     username: process.env.DB_USERNAME ? String(process.env.DB_USERNAME) : "",
     password: process.env.DB_PASSWORD ? String(process.env.DB_PASSWORD) : ""
