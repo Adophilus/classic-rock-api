216313a42e0b2b42d6ae232292ebf026cbb164da
Adophilus <uchenna19of@gmail.com>
2023-03-02 21:41:17 +0100

    updated the build command again

--- bin/build-all.sh
+++ bin/build-all.sh
@@ -1,7 +1,10 @@
 #! /usr/bin/env bash
 
 
-sudo apt-get install -y python3 python3-pip
+# sudo apt-get install -y python3 python3-pip
+
+python3 -m venv .venv
+source .venv/bin/activate
 
 pip3 install -r requirements.txt
 
