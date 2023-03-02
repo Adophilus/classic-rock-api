db6010ff9d28506e3b81bd91896ca3dbc4045ca3
Adophilus <uchenna19of@gmail.com>
2023-03-02 21:37:03 +0100

    updated the command to build the db

--- bin/build-all.sh
+++ bin/build-all.sh
@@ -3,11 +3,8 @@
 
 sudo apt-get install -y python3 python3-pip
 
-python3 -m venv .venv
-source .venv/bin/activate
-
 pip3 install -r requirements.txt
 
-pnpm build:db
+python3 -m scripts.serializer
 
 pnpm build
