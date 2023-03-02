b969386f2d3baf1218ce864b32e8e04cf8967aa0
Adophilus <uchenna19of@gmail.com>
2023-03-02 22:55:47 +0100

    added fix for renders python version

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,8 +5,10 @@ echo '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
 echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
 source ~/.bashrc
 
+# fix for render's python version
 asdf plugin-add python
 asdf install python 3.11.1
+asdf global python 3.11.1
 
 # python3 -m venv .venv
 # source .venv/bin/activate
