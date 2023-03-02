8fde57d5c9194ebfff2be7f1d432a049d8f6c6e6
Adophilus <uchenna19of@gmail.com>
2023-03-02 22:40:56 +0100

    updated the dockerfile

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env bash
 
-
-# sudo apt-get install -y python3 python3-pip
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y python3 python3-pip
 
 # python3 -m venv .venv
 # source .venv/bin/activate
